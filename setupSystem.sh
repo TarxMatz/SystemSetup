7bbe2db7c15ac0ce8d12d5140922b388474f3b1a
Saeto <Saeto>
2021-09-23 10:09:03 +0530

    added newline chars and placeholder option for trizen

--- setupSystem.sh
+++ setupSystem.sh
@@ -2,10 +2,17 @@
 
 echo "Installing packages..."
 
-sudo pacman -S --noconfirm xorg git firefox pcmanfm tlp rxvt-unicode ntfs-3g udiskie udisks2 feh alsa-utils pulseaudio lightdm lightdm-gtk-greeter lynis node-lts-fermium ttf-liberation network-manager-applet networkmanager-openvpn
+sudo pacman -S --noconfirm xorg git firefox pcmanfm tlp rxvt-unicode ntfs-3g udiskie udisks2 feh alsa-utils pulseaudio lightdm lightdm-gtk-greeter lynis node-lts-fermium ttf-liberation network-manager-applet networkmanager-openvpn dunst
 
 echo $(tput setaf 2) Package installation complete.
 echo $(tput sgr0)
+
+printf "\n"
+
+echo "Would you like to install trizen package manager from the aur? (yes/no):"
+read trizen
+
+printf "\n"
 echo "Enabling services..."
 
 sudo systemctl enable tlp
